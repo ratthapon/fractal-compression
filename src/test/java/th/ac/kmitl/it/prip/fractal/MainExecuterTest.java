a3956dbd421a0de91a792ccbb46b16b3cccf1fba
ratthapon_h <ratthapon_h@outlook.com>
2016-05-11 13:51:43 +0700

    Delete all test files and resources.

--- src/test/java/th/ac/kmitl/it/prip/fractal/MainExecuterTest.java
+++
@@ -1,9 +0,0 @@
-package th.ac.kmitl.it.prip.fractal;
-
-import org.junit.FixMethodOrder;
-import org.junit.runners.MethodSorters;
-
-@FixMethodOrder(MethodSorters.NAME_ASCENDING)
-public class MainExecuterTest {
-
-}
