10374cbcbfa93368a3f471741cc1bd1dd67782fd
ratthapon_h <ratthapon_h@outlook.com>
2016-08-24 01:18:24 +0700

    Add DataSet test suite.

---
+++ src/test/java/th/ac/kmitl/it/prip/fractal/DataSetTestSuite.java
@@ -0,0 +1,11 @@
+package th.ac.kmitl.it.prip.fractal;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
+
+@RunWith(Suite.class)
+@SuiteClasses({ AudioReadTest.class, AudioWriteTest.class, CodeReadTest.class, CodeWriteTest.class })
+public class DataSetTestSuite {
+
+}
