a5e1a10a457e9311cf3947cd909c26408ea8de2f
ratthapon_h <ratthapon_h@outlook.com>
2016-08-24 00:26:11 +0700

    Add unimplements compression test cases.

---
+++ src/test/java/th/ac/kmitl/it/prip/fractal/compression/audio/CompressorTest.java
@@ -0,0 +1,14 @@
+package th.ac.kmitl.it.prip.fractal.compression.audio;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class CompressorTest {
+
+	@Test
+	public void testCompress() {
+		fail("Not yet implemented");
+	}
+
+}
---
+++ src/test/java/th/ac/kmitl/it/prip/fractal/compression/audio/gpu/CUCompressorTest.java
@@ -0,0 +1,14 @@
+package th.ac.kmitl.it.prip.fractal.compression.audio.gpu;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class CUCompressorTest {
+
+	@Test
+	public void testCompress() {
+		fail("Not yet implemented");
+	}
+
+}
