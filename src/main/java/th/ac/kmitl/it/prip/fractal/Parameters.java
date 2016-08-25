157e1da822a1c43b1aa0c80fc5e3d601c08edd8a
ratthapon_h <ratthapon_h@outlook.com>
2016-08-25 22:09:53 +0700

    Fix unused import in Parameters.

--- src/main/java/th/ac/kmitl/it/prip/fractal/Parameters.java
+++ src/main/java/th/ac/kmitl/it/prip/fractal/Parameters.java
@@ -3,7 +3,6 @@ package th.ac.kmitl.it.prip.fractal;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
-import java.util.Arrays;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import java.util.regex.Matcher;
