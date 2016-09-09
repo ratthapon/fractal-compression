88d89965f9ab6e80c30778b659cf5cf2a38769dc
ratthapon_h <ratthapon_h@outlook.com>
2016-09-09 15:39:41 +0700

    Change verison number.

--- cmd-running.bat
+++ cmd-running.bat
@@ -1,1 +1,1 @@
-java -cp ".;lib/*;audio-compressor-0.0.1.jar" th.ac.kmitl.it.prip.fractal.MainExecuter F:\IFEFSR\SamplesSpeech\FC_params.txt
+java -cp ".;lib/*;audio-compressor-0.0.4.jar" th.ac.kmitl.it.prip.fractal.MainExecuter 
