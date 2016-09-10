cc82fcb65b32c03861bfcd85f94b4a3f248b246f
ratthapon_h <ratthapon_h@outlook.com>
2016-09-10 14:19:15 +0700

    Refactor memSetKernel.cu to use array indexing instead of dereference pointer.

--- cu/memSetKernel.cu
+++ cu/memSetKernel.cu
@@ -24,48 +24,48 @@ float **EP, float **SP
 		// initialize domain arrays
 		int dpOffset = (taskIdx * nRangeBlockSize * nCoeff);
 		for(int i = 0; i < nRangeBlockSize; i++){
-			*(DA + dpOffset + i) = 1.0f; // power 0
+			DA[dpOffset + i] = 1.0f; // power 0
 		}
 		for(int i = 0; i < nRangeBlockSize; i++){
-			*(DA + dpOffset + i + nRangeBlockSize) = 0.0f; // power 1
+			DA[dpOffset + i + nRangeBlockSize] = 0.0f; // power 1
 		}
 		// vec sumation
 		if(taskIdx < (nBatch/2)){
 			for(int i = 0; i < dScale; i++){
 				for(int j = 0; j < nRangeBlockSize; j++){
-					*(DA + dpOffset + nRangeBlockSize + j) = *(DA + dpOffset + nRangeBlockSize + j) + *(data + dColStart + j*dScale + i);
+					DA[dpOffset + nRangeBlockSize + j] = DA[dpOffset + nRangeBlockSize + j] + data[dColStart + j*dScale + i];
 				}
 			}
 		}else{ // gen reverse domain
 			for(int i = 0; i < dScale; i++){
 				for(int j = 0; j < nRangeBlockSize; j++){
-					*(DA + dpOffset + nRangeBlockSize + j) = *(DA + dpOffset + nRangeBlockSize + j) + *(dataRev + dColStart + j*dScale+ i);
+					DA[dpOffset + nRangeBlockSize + j] = DA[dpOffset + nRangeBlockSize + j] + dataRev[dColStart + j*dScale+ i];
 				}
 			}
 		}
 		// vec scalig
 		for(int j = 0; j < nRangeBlockSize; j++){
-			*(DA + dpOffset + nRangeBlockSize + j) = (*(DA + dpOffset + nRangeBlockSize + j))/dScale;
+			DA[dpOffset + nRangeBlockSize + j] = DA[dpOffset + nRangeBlockSize + j]/dScale;
 		}
 		// calculate next degree
 		for(int j = 2; j < nCoeff; j++){
 			int degreePad = (j*nRangeBlockSize);
 			for(int i = 0; i < nRangeBlockSize; i++){
-				*(DA + i + dpOffset + nRangeBlockSize + degreePad) = (*(DA + j + dpOffset + nRangeBlockSize)) * (*(DA + j + dpOffset + nRangeBlockSize + degreePad - nRangeBlockSize)) ; // power n>=2
+				DA[i + dpOffset + nRangeBlockSize + degreePad] = DA[j + dpOffset + nRangeBlockSize] * DA[j + dpOffset + nRangeBlockSize + degreePad - nRangeBlockSize] ; // power n>=2
 			}
 		}
 
 		// initialize range and error arrays
 		int rpOffset = (taskIdx * nRangeBlockSize);
 		for(int j = 0; j < nRangeBlockSize; j++){
-			*(RA + rpOffset + j) = *(R + j);
-			*(EA + rpOffset + j) = *(R + j);
+			RA[rpOffset + j] = R[j];
+			EA[rpOffset + j] = R[j];
 		}
 
 		// initialize covariance matrix with regularization
 		int apOffset = (taskIdx * nCoeff * nCoeff);
 		for(int i = 0; i < nCoeff * nCoeff; i+= nCoeff+1){
-			*(AA + apOffset + i) = regularize * regularize; // power 0
+			AA[apOffset + i] = regularize * regularize; // power 0
 		}
 
 		// pointing section
