0ccf299ff6e9255e902b487f37f4446bedc6ab47
ratthapon_h <ratthapon_h@outlook.com>
2016-12-05 18:46:43 +0700

    Refactor setDomainPoolKernel.cu.

--- cu/setDomainPoolKernel.cu
+++ cu/setDomainPoolKernel.cu
@@ -1,70 +1,112 @@
 extern "C"
 __global__ void setDomainPoolKernel(
-int nBatch,int rbs,int nDegree,int nDScale,int dBaseScale, float regularize,
+  int nBatch,int rbs,int nDegree,int nD,int dScale, float regularize,
 
-float *data,float *dataRev, // array of data and reverse data
-// arrays pointer
-float *DA,
-float *AA,
-float *IA,
-// pointer of array of pointer to pointer of array in arrays, nevermind i just stun you.
-// p(i) = data(i + size(data))
-float **DP,
-float **AP,
-float **IP
+  float *data,float *dataRev, // array of data and reverse data
+  // arrays pointer
+  float *DA,
+  float *AA,
+  float *IA,
+  // pointer of array of pointer to pointer of array in arrays, nevermind i just stun you.
+  // p(i) = data(i + size(data))
+  float **DP,
+  float **AP,
+  float **IP
 )
 {
-    int taskIdx = blockIdx.x * blockDim.x + threadIdx.x;
-    if (taskIdx < nBatch)
-    {
-		// initialize domain arrays
-		int nCoeff = ((nDegree - 1) * nDScale + 1);
-		int dpOffset = (taskIdx * rbs * nCoeff);
-		for(int i = 0; i < rbs; i++){
-			DA[dpOffset + i] = 1.0f; // power 0
-		}
-		for(int i = 0; i < rbs; i++){
-			DA[dpOffset + i + rbs] = 0.0f; // power 1
-		}
-		
-		int dStartIdx = taskIdx % (nBatch/2);
-		for(int ds = 1; ds <= nDScale; ds++){
-			// vec sumation
-			int mapDStart = dStartIdx + (nDScale - ds) * (rbs/2);
-			int dScale = dBaseScale * ds; // base_scale * current_scale
-			for(int i = 0; i < dScale; i++){
-				for(int j = 0; j < rbs; j++){
-					if(taskIdx < (nBatch/2)){
-						DA[dpOffset + rbs*ds + j] = DA[dpOffset + rbs*ds + j] + data[mapDStart + j*dScale + i];
-					}else{ // gen reverse domain
-						DA[dpOffset + rbs*ds + j] = DA[dpOffset + rbs*ds + j] + dataRev[mapDStart + j*dScale+ i];
-					}
-				}
-			}
-	
-			// vec scalig
-			for(int j = 0; j < rbs; j++){
-				DA[dpOffset + rbs*ds + j] = DA[dpOffset + rbs*ds + j]/dScale;
-			}
-		}
-		
-		// calculate next degree
-		for(int j = 2; j < nDegree; j++){
-			int degreePad = (j * rbs * nDScale );
-			for(int i = 0; i < rbs * nDScale; i++){
-				DA[i + dpOffset + rbs + degreePad] = DA[j + dpOffset + rbs] * DA[j + dpOffset + rbs + degreePad - rbs] ; // power n>=2
-			}
-		}
+  int taskIdx = blockIdx.x * blockDim.x + threadIdx.x;
+  if (taskIdx < nBatch){
+    // initialize domain arrays
+    // array structure
+    // DA = rbs-rows , 1 + D_1^1 + D_2^1 + D_3^1 + ... + D_ds^2
+    // nCoeff is power of Domains. start from power 0
+    // nDegree determine the number of degree (maximum degree)
+    // nD determine the number of domain blocks
+    // dScale determing the scale of domain size compare to rbs
+    // dpOffset determine the number of bytes padded for each Array
 
-		// initialize covariance matrix with regularization
-		int apOffset = (taskIdx * nCoeff * nCoeff);
-		for(int i = 0; i < nCoeff * nCoeff; i+= nCoeff+1){
-			AA[apOffset + i] = regularize * regularize; // power 0
-		}
+    int nCoeff = ((nDegree - 1) * nD + 1);
 
-		// pointing section
-		DP[taskIdx] = (DA + taskIdx * rbs * nCoeff);
-		AP[taskIdx] = (AA + taskIdx * nCoeff * nCoeff);
-		IP[taskIdx] = (IA + taskIdx * nCoeff * nCoeff);
+    // pointing array of pointers to array
+    const int daOffset = taskIdx * rbs * nCoeff;
+    const int aaOffset = taskIdx * nCoeff * nCoeff;
+    const int iaOffset = taskIdx * nCoeff * nCoeff;
+
+    DP[taskIdx] = &DA[daOffset];
+    AP[taskIdx] = &AA[aaOffset];
+    IP[taskIdx] = &IA[iaOffset];
+
+    // initialize covariance matrix with regularization
+    for(int i = 0; i < nCoeff * nCoeff; i++){
+      AA[aaOffset + i] = 0.0f;
     }
+    for(int i = 0; i < nCoeff * nCoeff; i+= nCoeff+1){
+      // set diagonal to regularization parameter
+      AA[aaOffset + i] = regularize * regularize;
+    }
+
+    // initialize first column covariance matrix
+    for(int i = 0; i < rbs; i++){
+      DA[daOffset + i] = 1.0f; // power 0
+    }
+
+    int dIdx = taskIdx % (nBatch/2);
+
+    // for each block number dn
+    for(int dn = 1; dn <= nD; dn++){
+      // set reference domain block
+      int dSize = rbs * dScale;
+      int dnIdx = dIdx + (dn - 1) * dSize; // * domian location factor
+
+      int rowDA = rbs*dn; // number of row of DA
+
+      // initialize column dn-th index
+      for(int i = 0; i < rbs; i++){
+        DA[daOffset + rowDA + i] = 0.0f; // power 1
+      }
+
+      // construct DA from domain blocks at power 1
+      // copy elements
+      for(int i = 0; i < rbs; i++){
+        if(taskIdx < (nBatch/2)){
+          DA[daOffset + rowDA + i] =
+          DA[daOffset + rowDA + i] + data[dnIdx + i*dScale];
+        }else{ // gen reverse domain
+          DA[daOffset + rowDA + i] =
+          DA[daOffset + rowDA + i] + dataRev[dnIdx + i*dScale];
+        }
+      }
+
+      // handling if domain blocks are larger than rbs (by downsample)
+      for(int ds = 1; ds < dScale; ds++){
+        // vec sumation
+        for(int i = 0; i < rbs; i++){
+          if(taskIdx < (nBatch/2)){
+            DA[daOffset + rowDA + i] =
+            DA[daOffset  + rowDA + i] + data[dnIdx + ds + i*dScale];
+          }else{ // gen reverse domain
+            DA[daOffset + rowDA + i] =
+            DA[daOffset  + rowDA + i] + dataRev[dnIdx + ds + i*dScale];
+          }
+        }
+      }
+
+      // vec scalig after resample
+      for(int i = 0; i < rbs; i++){
+        DA[daOffset + rowDA + i] = DA[daOffset + rowDA + i]/dScale;
+      }
+
+      // calculate next degree
+      // for(int deg = 2; deg <= nDegree; deg++){
+      //   int degPad = rowDA * nD * (deg - 2);
+      //   int nextDegPad = rowDA * nD * (deg - 1);
+      //   for(int i = 0; i < rbs * nD; i++){
+      //     // power n>=2
+      //     // D^n = D^1 * D^(n-1)
+      //     DA[daOffset + nextDegPad + rbs + i] =
+      //     DA[daOffset + rbs + i] * DA[daOffset + degPad + rbs + i] ;
+      //   }
+      // }
+    }
+  }
 }
