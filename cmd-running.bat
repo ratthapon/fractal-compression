4a619b71c6f360dd761bf9e4d5033d62258ef823
ratthapon_h <ratthapon_h@outlook.com>
2017-06-06 09:35:05 +0700

    Delete unrelated files, including last-version libs, cmd-running.bat, 1st ctx-compile-script

--- cmd-running.bat
+++
@@ -1,1 +0,0 @@
-java -cp ".;lib/*;audio-compressor-0.0.4.jar" th.ac.kmitl.it.prip.fractal.MainExecuter 
--- ctx-compile-script.bat
+++
@@ -1,5 +0,0 @@
-nvcc -ptx cu/reverseVec.cu -o ./src/main/resources/reverseVec.ptx
-nvcc -ptx cu/memSetKernel.cu -o ./src/main/resources/memSetKernel.ptx
-nvcc -ptx cu/limitCoeff.cu -o ./src/main/resources/limitCoeff.ptx
-nvcc -ptx cu/sumSquareError.cu -o ./src/main/resources/sumSquareError.ptx
-pause > complete compile ctx
